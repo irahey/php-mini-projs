28a826f712c6dc5020baaf47d5e112a05d9fb600
Ira Garcia <iraolivia.garcia@yahoo.com>
2018-02-13 13:40:10 +0000

    started with an ajax chatbox

--- chat-app/script.js
+++ chat-app/script.js
@@ -1,63 +1,35 @@
- // ask user for name with popup prompt    
-        var name = prompt("Enter your chat name:", "Guest");
-        
-        // default name is 'Guest'
-    	if (!name || name === ' ') {
-    	   name = "Guest";	
-    	}
-    	
-    	// strip tags
-    	name = name.replace(/(<([^>]+)>)/ig,"");
-    	
-    	// display name on page
-    	$("#name-area").html("You are: <span>" + name + "</span>");
-    	
-    	// kick off chat
-        var chat =  new Chat();
-    	$(function() {
-    	
-    		 chat.getState(); 
-    		 
-    		 // watch textarea for key presses
-             $("#sendie").keydown(function(event) {  
-             
-                 var key = event.which;  
-           
-                 //all keys including return.  
-                 if (key >= 33) {
-                   
-                     var maxLength = $(this).attr("maxlength");  
-                     var length = this.value.length;  
-                     
-                     // don't allow new content if length is maxed out
-                     if (length >= maxLength) {  
-                         event.preventDefault();  
-                     }  
-                  }  
-    		 																																																});
-    		 // watch textarea for release of key press
-    		 $('#sendie').keyup(function(e) {	
-    		 					 
-    			  if (e.keyCode == 13) { 
-    			  
-                    var text = $(this).val();
-    				var maxLength = $(this).attr("maxlength");  
-                    var length = text.length; 
-                     
-                    // send 
-                    if (length <= maxLength + 1) { 
-                     
-    			        chat.send(text, name);	
-    			        $(this).val("");
-    			        
-                    } else {
-                    
-    					$(this).val(text.substring(0, maxLength));
-    					
-    				}	
-    				
-    				
-    			  }
-             });
-            
-    	});
+/* most simple ajax chat script (www.linuxuser.at) (GPLv2) */
+var nick_maxlength=10;
+var http_request=false;
+var http_request2=false;
+var intUpdate;
+/* http_request for writing */
+function ajax_request(url){http_request=false;if(window.XMLHttpRequest){http_request=new XMLHttpRequest();if(http_request.overrideMimeType){http_request.overrideMimeType('text/xml');}}else if(window.ActiveXObject){try{http_request=new ActiveXObject("Msxml2.XMLHTTP");}catch(e){try{http_request=new ActiveXObject("Microsoft.XMLHTTP");}catch(e){}}}
+if(!http_request){alert('Giving up :( Cannot create an XMLHTTP instance');return false;}
+http_request.onreadystatechange=alertContents;http_request.open('GET',url,true);http_request.send(null);}
+function alertContents(){if(http_request.readyState==4){if(http_request.status==200){rec_response(http_request.responseText);}else{}}}
+/* http_request for reading */
+function ajax_request2(url){http_request2=false;if(window.XMLHttpRequest){http_request2=new XMLHttpRequest();if(http_request2.overrideMimeType){http_request2.overrideMimeType('text/xml');}}else if(window.ActiveXObject){try{http_request2=new ActiveXObject("Msxml2.XMLHTTP");}catch(e){try{http_request2=new ActiveXObject("Microsoft.XMLHTTP");}catch(e){}}}
+if(!http_request2){alert('Giving up :( Cannot create an XMLHTTP instance');return false;}
+http_request2.onreadystatechange=alertContents2;http_request2.open('GET',url,true);http_request2.send(null);}
+function alertContents2(){if(http_request2.readyState==4){if(http_request2.status==200){rec_chatcontent(http_request2.responseText);}else{}}}
+/* chat stuff */
+message.focus()
+var show_newmsg_on_bottom=1;     /* set to 0 to let new msg´s appear on top */
+var waittime=3000;        /* time between chat refreshes (ms) */
+intUpdate=window.setTimeout("read_cont();", waittime);
+chatbox.value = "loading...";
+function read_cont()         { zeit = new Date(); ms = (zeit.getHours() * 24 * 60 * 1000) + (zeit.getMinutes() * 60 * 1000) + (zeit.getSeconds() * 1000) + zeit.getMilliseconds(); ajax_request2("chat.txt?x=" + ms); }
+function display_msg(msg1)     { chatbox.value = msg1.trim(); }
+function keyup(arg1)         { if (arg1 == 13) submit_msg(); }
+function submit_msg()         { clearTimeout(intUpdate); if (nickname.value == "") { check = prompt("please enter username:"); if (check === null) return 0; if (check == "") check="..."; nickname.value=check; } if (nickname.value.length > nick_maxlength) nickname.value=nickname.value.substring(0,nick_maxlength); spaces=""; for(i=0;i<(nick_maxlength-nickname.value.length);i++) spaces+=" "; v=chatbox.value.substring(chatbox.value.indexOf("\n")) + "\n" + nickname.value + spaces + "| " + message.value; if (message.value != "") chatbox.value=v.substring(1); write_msg(message.value,nickname.value); message.value=""; intUpdate=window.setTimeout("read_cont();", waittime);}
+function write_msg(msg1,nick1)     { ajax_request("w.php?m=" + escape(msg1) + "&n=" + escape(nick1)); }
+function rec_response(str1)     { }
+function rec_chatcontent(cont1) {
+    if (cont1 != "") {
+        out1 = unescape(cont1);
+        if (show_newmsg_on_bottom == 0) { out1 = ""; while (cont1.indexOf("\n") > -1) { out1 = cont1.substr(0, cont1.indexOf("\n")) + "\n" + out1; cont1 = cont1.substr(cont1.indexOf("\n") + 1); out1 = unescape(out1); } }
+        if (chatbox.value != out1) { display_msg(out1); }
+        intUpdate=window.setTimeout("read_cont()", waittime);
+    }
+}
